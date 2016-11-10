1280ff4eb934ac6873a9699f88b84849a54462fc
calvin.zhu <czhu@redhat.com>
2016-11-10 13:47:02 -0500

    test

---
+++ nodejs_product/product.js
@@ -0,0 +1,358 @@
+var express = require('express');
+var app     = express();
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+
+var mysql  = require('mysql');
+
+var dbHost = process.env.MYSQL_HOST,
+      dbUser = process.env.MYSQL_USER,
+      dbDatabase = process.env.MYSQL_DATABASE,
+      dbPassword = process.env.MYSQL_PASSWORD;
+
+
+console.log('!!!!!!!!!!!!!!!!!!!!!!dbHost ' + dbHost);
+console.log('!!!!!!!!!!!!!!!!!!!!!!dbUser ' + dbUser);
+console.log('!!!!!!!!!!!!!!!!!!!!!!dbDatabase ' + dbDatabase);
+console.log('!!!!!!!!!!!!!!!!!!!!!!dbPassword ' + dbPassword);
+
+
+
+
+app.get('/', function(req, res) {
+	var result = [
+	  { status : "test worked"}
+	];
+  res.json(result);
+});
+
+app.get('/product/version', function(req, res) {
+	var result = [
+	  { version : "1"}
+	];
+  res.json(result);
+});
+
+
+//get either featured products or products with keyword
+app.get('/product/products', function(req, httpRes) {
+
+	var dbconn = mysql.createConnection({
+	  host     : dbHost,
+	  user     : dbUser,
+	  password : dbPassword,
+	  database : dbDatabase
+	});
+	dbconn.connect(function(err){
+	  if(err){
+	    console.log('Database connection error');
+	  }else{
+	    console.log('Database connection successful');
+	  }
+	});
+
+
+	if(req.query.featured == null && req.query.keyword == null) {
+		httpRes.statusCode = 400;
+		return httpRes.send('All products cannot be returned, need to provide a search condition');
+	}
+
+	if (req.query.featured != null) {
+
+		dbconn.query('select sku, availability, description, featured=1 as featured, height, image, length, name, price, weight, width from Product where featured=true', function(err, records){
+//		dbconn.query('SELECT * FROM Product where featured=true', function(err, records){
+		  if(err) throw err;
+		  httpRes.json(records);
+		});
+	} else if (req.query.keyword != null){
+		dbconn.query('select sku, availability, description, featured=1 as featured, height, image, length, name, price, weight, width from Product where SKU in (select SKU from PRODUCT_KEYWORD where Keyword = ?)', req.query.keyword, function(err, records){
+		  if(err) throw err;
+		  httpRes.json(records);
+		});
+	} 
+
+	dbconn.end(function(err) {
+	    console.log('Database connection is end');
+	});
+
+
+
+});
+
+
+//get based on sku #
+app.get('/product/products/:sku', function(req, httpRes) {
+
+	var dbconn = mysql.createConnection({
+	  host     : dbHost,
+	  user     : dbUser,
+	  password : dbPassword,
+	  database : dbDatabase
+	});
+	dbconn.connect(function(err){
+	  if(err){
+	    console.log('Database connection error');
+	  }else{
+	    console.log('Database connection successful');
+	  }
+	});
+
+	console.log('!!!!!!!!!!!here222');
+	dbconn.query('select sku, availability, description, featured=1 as featured, height, image, length, name, price, weight, width from Product where SKU = ? ', req.params.sku, function(err, records){
+		console.log('!!!!!!!!!!!here33' + records[0]);
+	  if(err) throw err;
+	console.log('!!!!!!!!!!!here33');
+	  httpRes.json(records[0]);
+	console.log('!!!!!!!!!!!here44');
+	});
+
+	dbconn.end(function(err) {
+	    console.log('Database connection is end');
+	});
+
+});
+
+
+//add product through post 
+app.post('/product/products', function(req, httpRes) {
+	if(!req.body.hasOwnProperty('DESCRIPTION') || !req.body.hasOwnProperty('NAME')) {
+		httpRes.statusCode = 400;
+		return httpRes.send('Error 400: need to have valid DESCRIPTION and NAME.');
+	}
+
+	var dbconn = mysql.createConnection({
+	  host     : dbHost,
+	  user     : dbUser,
+	  password : dbPassword,
+	  database : dbDatabase
+	});
+
+
+	dbconn.connect(function(err){
+	  if(err){
+	    console.log('Database connection error');
+	  }else{
+	    console.log('Database connection successful');
+	  }
+	});
+
+	var record= { DESCRIPTION: req.body.DESCRIPTION, HEIGHT: req.body.HEIGHT, LENGTH: req.body.LENGTH,  NAME: req.body.NAME, WEIGHT: req.body.WEIGHT, WIDTH: req.body.WIDTH, FEATURED: req.body.FEATURED, AVAILABILITY: req.body.AVAILABILITY, IMAGE: req.body.IMAGE, PRICE: req.body.PRICE};
+
+
+	dbconn.query('INSERT INTO Product SET ?', record, function(err,dbRes){
+		if(err) throw err;
+		console.log('Last record insert into Product table, SKU:', dbRes.insertId);
+		var sku = dbRes.insertId;
+
+		record = {KEYWORD: req.body.IMAGE, SKU: dbRes.insertId};
+		dbconn.query('INSERT INTO PRODUCT_KEYWORD SET ?', record, function(err,dbRes){
+			if(err) {throw err;}
+			else {
+				console.log('record insert into PRODUCT_KEYWORD table');
+				result = [
+				  { status : "SUCCESS",
+				  SKU : sku}
+				];
+				httpRes.json(result);
+			}
+
+			dbconn.end(function(err) {
+			    console.log('Database connection is end');
+			});
+
+		});
+	});
+
+
+
+
+});
+
+//reduce product through post, this is for the checkout process 
+app.post('/product/reduce', function(req, httpRes) {
+
+	var array = req.body.length;
+	console.log('!!!!!!!!!!!!!reduce array length: ' + req.body.length);
+
+
+	var dbconn = mysql.createConnection({
+	  host     : dbHost,
+	  user     : dbUser,
+	  password : dbPassword,
+	  database : dbDatabase
+	});
+
+
+	var sendReply = false;
+	for (var i = 0; i < req.body.length; i++) {
+		if(!req.body[i].hasOwnProperty('sku') || !req.body[i].hasOwnProperty('quantity')) {
+			httpRes.statusCode = 400;
+			return httpRes.send('Error 400: need to have valid sku and quantity.');
+		}
+
+
+		var tmpSku = req.body[i]['sku'];
+		var tmpQuantity = req.body[i]['quantity'];
+	    	console.log('!!!!!!!!!!!!!reduce sku ' + tmpSku.toString());
+	    	console.log('!!!!!!!!!!!!!reduce quantity ' + tmpQuantity.toString());
+		var sqlStr = 'update Product set availability = availability - ' + tmpQuantity + ' where sku = ' + tmpSku + ' and availability - ' + tmpQuantity + ' > 0'; 
+
+		dbconn.query(sqlStr, function(err, result){
+			if(err)  { 
+				console.log('!!!!!!!!!!err ' + err);
+				throw err;
+
+			}
+		  	if (result.affectedRows > 0) {
+				console.log('reduced from Product ' + result.affectedRows + ' rows');
+		  	} else {
+				var result = [
+				  { message : 'Insufficient availability for ' + tmpSku,
+				  details : null}
+				];
+				if (sendReply == false) {
+					httpRes.json(result);
+					sendReply = true;
+				}
+
+		  	}
+		});
+
+	}
+
+	dbconn.end(function(err) {
+	    console.log('Database connection is end');
+	});
+
+	return httpRes.send('');;
+
+
+});
+
+//delete based on sku #
+app.delete('/product/products/:sku', function(req, httpRes) {
+
+	var dbconn = mysql.createConnection({
+	  host     : dbHost,
+	  user     : dbUser,
+	  password : dbPassword,
+	  database : dbDatabase
+	});
+	dbconn.connect(function(err){
+	  if(err){
+	    console.log('Database connection error');
+	  }else{
+	    console.log('Database connection successful');
+	  }
+	});
+
+	dbconn.query('DELETE FROM PRODUCT_KEYWORD where SKU = ?', req.params.sku, function(err, result){
+	  if(err) throw err;
+		console.log('deleted from PRODUCT_KEYWORD ' + result.affectedRows + ' rows');
+	});
+
+	dbconn.query('DELETE FROM Product where SKU = ?', req.params.sku, function(err, result){
+	  if(err) throw err;
+		console.log('deleted from Product ' + result.affectedRows + ' rows');
+	});
+
+  	httpRes.json('deleted from both Product and PRODUCT_KEYWORD tables');
+
+	dbconn.end(function(err) {
+	    console.log('Database connection is end');
+	});
+
+});
+
+//put (update) based on sku #
+app.put('/product/products/:sku', function(req, res) {
+	console.log('!!!!!!!!!!calling put method');
+	updateProduct(req.params.sku, req, res);
+});
+
+//patch (update) based on sku #
+app.patch('/product/products/:sku', function(req, res) {
+	console.log('!!!!!!!!!!!calling patch method');
+	updateProduct(req.params.sku, req, res);
+});
+
+
+//real update function works for both put and patch request
+function updateProduct(skuIn, req, httpRes) {
+
+	var dbconn = mysql.createConnection({
+	  host     : dbHost,
+	  user     : dbUser,
+	  password : dbPassword,
+	  database : dbDatabase
+	});
+	dbconn.connect(function(err){
+	  if(err){
+	    console.log('Database connection error');
+	  }else{
+	    console.log('Database connection successful');
+	  }
+	});
+
+	var sqlStr0 = 'UPDATE Product SET '; 
+	var sqlStr = ''; 
+	if (req.body.DESCRIPTION != null){
+		sqlStr = 'DESCRIPTION = \'' + req.body.DESCRIPTION + "\'";
+	}
+	if (req.body.HEIGHT != null){
+		if (sqlStr !='') sqlStr = sqlStr + " , ";
+		sqlStr = sqlStr + 'HEIGHT = \'' + req.body.HEIGHT + "\'";
+	}
+	if (req.body.LENGTH != null){
+		if (sqlStr !='') sqlStr = sqlStr + " , ";
+		sqlStr = sqlStr + 'LENGTH = \'' + req.body.LENGTH + "\'";
+	}
+	if (req.body.NAME != null){
+		if (sqlStr !='') sqlStr = sqlStr + " , ";
+		sqlStr = sqlStr + 'NAME = \'' + req.body.NAME + "\'";
+	}
+	if (req.body.WEIGHT != null){
+		if (sqlStr !='') sqlStr = sqlStr + " , ";
+		sqlStr = sqlStr + 'WEIGHT = \'' + req.body.WEIGHT + "\'";
+	}
+	if (req.body.WIDTH != null){
+		if (sqlStr !='') sqlStr = sqlStr + " , ";
+		sqlStr = sqlStr + 'WIDTH = \'' + req.body.WIDTH + "\'";
+	}
+	if (req.body.FEATURED != null){
+		if (sqlStr !='') sqlStr = sqlStr + " , ";
+		sqlStr = sqlStr + 'FEATURED = \'' + req.body.FEATURED + "\'";
+	}
+	if (req.body.AVAILABILITY != null){
+		if (sqlStr !='') sqlStr = sqlStr + " , ";
+		sqlStr = sqlStr + 'AVAILABILITY = \'' + req.body.AVAILABILITY + "\'";
+	}
+	if (req.body.IMAGE != null){
+		if (sqlStr !='') sqlStr = sqlStr + " , ";
+		sqlStr = sqlStr + 'IMAGE = \'' + req.body.IMAGE + "\'";
+	}
+	if (req.body.PRICE != null){
+		if (sqlStr !='') sqlStr = sqlStr + " , ";
+		sqlStr = sqlStr + 'PRICE = \'' + req.body.PRICE + "\'";
+	}
+	sqlStr = sqlStr0 + sqlStr + ' WHERE SKU = ?';
+    	console.log('!!!!!SQL ready to be executed: ' + sqlStr);
+
+
+	dbconn.query(sqlStr, skuIn, function(err, result){
+	  if(err) throw err;
+		console.log('update Product table' + result.affectedRows + ' rows');
+	});
+
+  	httpRes.json('Update Product table');
+
+	dbconn.end(function(err) {
+	    console.log('Database connection is end');
+	});
+
+}
+
+
+
+app.listen(process.env.PORT || 8080);
---
+++ ruby_billing/config.ru
@@ -0,0 +1,85 @@
+require 'bundler/setup'
+require 'json'
+require_relative 'lib/result'
+require_relative 'lib/transcation'
+
+Bundler.require(:default)
+
+class Application < Sinatra::Base
+
+
+  before do
+    content_type 'application/json'
+  end
+
+
+  get '/' do
+    ENV.inspect
+  end
+
+get '/billing/process' do
+  "please use post method on url/process to handle credit card transaction"
+end
+
+
+
+
+
+
+get '/billing/refund/:transactionNumber' do
+	"refund for '#{params[:transactionNumber]}'"
+end
+
+
+post '/billing/refund/:id' do
+	"refund for transcation number #{params[:id]}"
+end
+
+post '/billing/process' do
+ begin
+  post_data =  JSON.parse request.body.read
+  if post_data.nil? or !post_data.has_key?('creditCardNumber')  or !post_data.has_key?('verificationCode')
+   puts "ERROR, no CC# or verificatoinCode!!!!!!!!"
+  else
+   	transcation = Transcation.new 
+	transcation.creditCardNumber = post_data['creditCardNumber']
+	transcation.expMonth = post_data['expMonth']
+	transcation.expYear = post_data['expYear']
+	transcation.verificationCode = post_data['verificationCode']
+	transcation.billingAddress = post_data['billingAddress']
+	transcation.customerName = post_data['customerName']
+	transcation.orderNumber = post_data['orderNumber']
+	transcation.amount = post_data['amount']
+
+	  puts "creditCardNumber  #{transcation.creditCardNumber}"
+	  puts "expMonth          #{transcation.expMonth}"
+	  puts "expYear           #{transcation.expYear}"
+	  puts "billingAddress    #{transcation.billingAddress}"
+	  puts "customerName      #{transcation.customerName}"
+	  puts "orderNumber       #{transcation.orderNumber}"
+	  puts "amount            #{transcation.amount}"
+
+	result = Result.new
+	result.name = transcation.customerName
+	result.orderNumber = transcation.orderNumber
+	result.transactionDate = DateTime.now
+	result.transactionNumber = 9000000 + rand(1000000)
+
+	if transcation.expYear.to_i < Time.now.year.to_i or (transcation.expYear.to_i == Time.now.year.to_i and transcation.expMonth.to_i <= Time.now.month.to_i)
+		result.status = "FAILURE"
+	else
+		result.status = "SUCCESS"
+	end
+
+	result.to_json
+
+  end
+ end
+end
+
+
+
+
+end
+
+run Application
